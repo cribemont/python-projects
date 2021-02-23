ed1effe87140bf1c73f9a3471693c30ff13105b6
cribemont <charles.ribemont@gmail.com>
2021-02-23 16:41:20 +0100

    Completing Day 1

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,2 +1,32 @@
-print("Ok, this is very super basic")
-print("And multiline wow !")
+#print("Ok, this is very super basic")
+#print("And multiline wow !")
+#print("Cool string" + " " + "concatenation")
+
+# ask user input
+#print(" Yo " + input("So what's your name ? ") + "!")
+
+# get the length of the input string // CANT concatenate with + and str
+#print( len( input("So what's your name ? ") ) )
+
+# assign to variable
+#user_name = input("Username : ")
+#print( user_name )
+
+# variable switching ?
+#a = input("a: ")
+#b = input("b: ")
+
+#temp_a = a
+#a = b
+#b = temp_a
+
+#print("a is now:" + a)
+#print("b is now:" + b)
+
+# Final ex: name generator
+city_name = input("City name :\n")
+pet_name = input("Pet name :\n")
+
+band_name = city_name + " " + pet_name
+
+print("Your band name is " + band_name)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Python Projects
 
 Training Python projects during multiple days of code
+
+# Day 1
+Basic stuff reviewing print / input and basic variable string manipulation
